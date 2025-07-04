258fc3aa73d90dee332b9399c94639e2078d5f1d
Serhii Rudenko <serhii.rudenko@prozorro.sale>
2025-07-04 13:58:19 +0300

    init

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # kubebuilder_test
+
+kubebuilder init --domain sr.ios.in.ua --plugins go/v4 --repo github.com/redacid/kubebuilder_test --project-name=redacid-test
+kubebuilder create api --group prozorro --version v1alpha1 --kind MapRole
+kubebuilder create api --group prozorro --version v1alpha1 --kind MapUser
+kubebuilder create api --group prozorro --version v1alpha1 --kind MapAccount
---
+++ internal/controller/mapaccount_controller.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2025.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+
+	prozorrov1alpha1 "github.com/redacid/kubebuilder_test/api/v1alpha1"
+	"github.com/redacid/kubebuilder_test/awsauth"
+	"github.com/redacid/kubebuilder_test/kube"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+// MapAccountReconciler reconciles a MapAccount object
+type MapAccountReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+// +kubebuilder:rbac:groups=prozorro.sr.ios.in.ua,resources=mapaccounts,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=prozorro.sr.ios.in.ua,resources=mapaccounts/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=prozorro.sr.ios.in.ua,resources=mapaccounts/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the MapAccount object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.21.0/pkg/reconcile
+func (r *MapAccountReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	_ = logf.FromContext(ctx)
+
+	// TODO(user): your logic here
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *MapAccountReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&prozorrov1alpha1.MapAccount{}).
+		Named("mapaccount").
+		Complete(r)
+}
